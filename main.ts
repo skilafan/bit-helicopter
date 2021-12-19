2e85c82c479d5cbf7f2c59fcdcb243a74651fa4c
Skila FAN <96375466+skilafan@users.noreply.github.com>
2021-12-19 21:06:44 +0800

    增加 bluetooth uart 控制电机开关的功能

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
+bluetooth.onBluetoothConnected(function () {
+    isBluetoothConnected = 1
+    for (let index = 0; index <= 2; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(500)
+    }
+})
+bluetooth.onBluetoothDisconnected(function () {
+    isBluetoothConnected = 0
+    SuperBit.MotorStopAll()
+    basic.showIcon(IconNames.Asleep)
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
+    command = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Space))
+})
 input.onGesture(Gesture.Shake, function () {
     latestShakeTime = input.runningTime()
 })
 let motor1 = 0
 let latestShakeTime = 0
+let command = ""
+let isBluetoothConnected = 0
 basic.showLeds(`
     . . . . .
     . # . # .
@@ -10,13 +29,25 @@ basic.showLeds(`
     . # . # .
     . . . . .
     `)
+bluetooth.startUartService()
 basic.pause(100)
 basic.showIcon(IconNames.Asleep)
 basic.forever(function () {
-    if (input.runningTime() - latestShakeTime < 3000) {
-        motor1 = 1
-    } else {
+    if (isBluetoothConnected == 0) {
         motor1 = 0
+    } else {
+        if (command == "#GO") {
+            motor1 = 1
+        } else if (command == "#STOP") {
+            motor1 = 0
+        } else {
+            motor1 = 0
+        }
+        if (input.runningTime() - latestShakeTime < 3000) {
+            motor1 = 1
+        } else {
+            motor1 = 0
+        }
     }
     if (motor1 == 1) {
         SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
